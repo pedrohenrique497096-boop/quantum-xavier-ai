c11a2035a0ed9632564ecb7e461d91017444f4d4
pedrohenrique497096-boop <pedrohenrique497096@gmail.com>
2026-03-08 10:51:53 -0300

    Update model.py

--- ai/model.py
+++ ai/model.py
@@ -1,1 +1,18 @@
+from sklearn.ensemble import RandomForestClassifier
 
+class MarketAI:
+
+    def __init__(self):
+
+        self.model = RandomForestClassifier(
+            n_estimators=600,
+            max_depth=14
+        )
+
+    def train(self,X,y):
+
+        self.model.fit(X,y)
+
+    def predict(self,X):
+
+        return self.model.predict_proba(X)
