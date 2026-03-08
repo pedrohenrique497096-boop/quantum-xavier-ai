2de31d0e3bce566c7cf82c48333bf1dff31170ae
pedrohenrique497096-boop <pedrohenrique497096@gmail.com>
2026-03-08 10:50:16 -0300

    Update indicators.py

--- core/indicators.py
+++ core/indicators.py
@@ -1,1 +1,18 @@
+def ema(series,length):
 
+    return series.ewm(span=length).mean()
+
+
+def rsi(series,length=14):
+
+    delta = series.diff()
+
+    gain = delta.clip(lower=0)
+    loss = -delta.clip(upper=0)
+
+    avg_gain = gain.rolling(length).mean()
+    avg_loss = loss.rolling(length).mean()
+
+    rs = avg_gain / avg_loss
+
+    return 100 - (100/(1+rs))
