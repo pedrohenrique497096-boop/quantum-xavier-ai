8c4e15773eb10d442290d5fcfbafb5ba0997d7c6
pedrohenrique497096-boop <pedrohenrique497096@gmail.com>
2026-03-08 08:55:57 -0300

    Update scanner.py

--- core/scanner.py
+++ core/scanner.py
@@ -1,1 +1,23 @@
+from data.market import get_data
+from core.engines import breakout_engine, reversal_engine, continuation_engine
+from core.signals import build_signal
 
+def scan(symbol):
+
+    df = get_data(symbol)
+
+    if df is None:
+        return None
+
+    breakout = breakout_engine(df)
+
+    rev_buy, rev_sell = reversal_engine(df)
+
+    cont_buy, cont_sell = continuation_engine(df)
+
+    buy_score = breakout + rev_buy + cont_buy
+    sell_score = breakout + rev_sell + cont_sell
+
+    signal = build_signal(symbol, df, buy_score, sell_score)
+
+    return signal
