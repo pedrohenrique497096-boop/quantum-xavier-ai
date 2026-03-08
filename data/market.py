1c858fce5c3145659766cbaf24a4fe3a76bbe78f
pedrohenrique497096-boop <pedrohenrique497096@gmail.com>
2026-03-08 08:54:18 -0300

    Update market.py

--- data/market.py
+++ data/market.py
@@ -1,1 +1,17 @@
+import yfinance as yf
 
+def get_data(symbol, interval="5m", bars=300):
+
+    df = yf.download(
+        symbol,
+        interval=interval,
+        period="7d",
+        progress=False
+    )
+
+    if df is None or df.empty:
+        return None
+
+    df = df.tail(bars)
+
+    return df
