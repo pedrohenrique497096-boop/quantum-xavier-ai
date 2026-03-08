0d0b60427f0ec4c5bc82e973792000b5e7955839
pedrohenrique497096-boop <pedrohenrique497096@gmail.com>
2026-03-08 10:52:50 -0300

    Update trades.py

--- database/trades.py
+++ database/trades.py
@@ -1,1 +1,24 @@
+import sqlite3
 
+DB="trades.db"
+
+def setup():
+
+    conn = sqlite3.connect(DB)
+
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS trades(
+        id INTEGER PRIMARY KEY,
+        symbol TEXT,
+        direction TEXT,
+        entry REAL,
+        stop REAL,
+        target REAL,
+        result TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
