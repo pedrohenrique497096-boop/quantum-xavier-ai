0dc47525028f4350d2feed249cb0858761e6b35d
pedrohenrique497096-boop <pedrohenrique497096@gmail.com>
2026-03-10 12:05:58 -0300

    Update risk.py

--- core/risk.py
+++ core/risk.py
@@ -1,1 +1,18 @@
+def calculate_risk(price):
 
+    stop=price*0.995
+
+    tp1=price*1.005
+
+    tp2=price*1.01
+
+    tp3=price*1.02
+
+    return {
+
+        "stop":stop,
+        "tp1":tp1,
+        "tp2":tp2,
+        "tp3":tp3
+
+    }
