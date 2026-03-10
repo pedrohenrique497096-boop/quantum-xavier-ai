4dbda83a18031e7b2d8400950747912c7084c1bf
pedrohenrique497096-boop <pedrohenrique497096@gmail.com>
2026-03-10 13:31:09 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,16 +1,13 @@
 BINANCE_API = "https://api.binance.com/api/v3"
-
 TWELVEDATA_API = "https://api.twelvedata.com"
 
 TWELVEDATA_KEY = "1bbb904ae0994fb7b2d120da18c66602"
 
 ASSETS = [
-"BTCUSD",
-"XAUUSD",
-"EURUSD",
-"GBPUSD",
-"USDJPY",
-"EURJPY"
+    "BTCUSD",
+    "XAUUSD",
+    "EURUSD",
+    "GBPUSD",
+    "USDJPY",
+    "EURJPY"
 ]
-
-SCAN_INTERVAL = 10
